fcacde71a6f1576744c77ddcdc1e0c5a25d5294c
Nick Klassen <nick.klassen@smail.th-koeln.de>
2022-07-25 13:33:29 +0200

    created ans tested objects

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,38 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+const computer = {
+
+    price : 199,
+    name : "acer",
+    displaySize : 15,
+    power : 10,
+    isOn : true
+};
+
+
+const car = {
+
+    brand : "mercedes",
+    year : 2014,
+    numberOfSeats : 5,
+    price : 200.000
+};
+
+const water = {
+
+    color : "transparent",
+    isSweet : false,
+};
+
+
+const bottle = {
+
+    content : water,
+    size : 25
+};
+
+console.log(water);
+console.log(bottle.content);
