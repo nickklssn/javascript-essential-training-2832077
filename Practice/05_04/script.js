c037404a458da88707cba7c6f7149a98e56ee296
Nick Klassen <nick.klassen@smail.th-koeln.de>
2022-07-26 13:37:38 +0200

    tested queries

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,16 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+const but = document.querySelectorAll("button[class = lid-toggle]");
+
+const list = document.querySelector("[class = feature_backpack__strap][data-side = right]")
+
+const div = document.querySelector("header div:last-child").innerHTML
+
+
+console.log(list);
+console.log(but);
+console.log(div);
+
+
