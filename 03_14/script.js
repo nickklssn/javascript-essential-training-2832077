d98b73060cd3e6bf9d3ccae0ff8732735630b744
Nick Klassen <nick.klassen@smail.th-koeln.de>
2022-07-26 11:01:27 +0200

    completed challenge creating new object type

--- 03_14/script.js
+++ 03_14/script.js
@@ -6,6 +6,8 @@
  */
 
 import Backpack from "./Backpack.js";
+import Book from "./Book.js";
+
 
 const everydayPack = new Backpack(
   "Everyday Backpack",
@@ -18,6 +20,22 @@ const everydayPack = new Backpack(
   "December 5, 2018 15:00:00 PST"
 );
 
+const harryPotter = new Book(
+  "Harry Potter",
+  2001,
+  "Rowling"
+);
+
+const theBoys = new Book(
+  "The Boys",
+  2022,
+  "Nick"
+);
+
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
 console.log("Days since aquired:", everydayPack.backpackAge());
+console.log(harryPotter);
+console.log(harryPotter.author);
+console.log(theBoys);
+console.log(theBoys.author);
