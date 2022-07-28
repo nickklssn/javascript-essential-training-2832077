54286c2152bf1a514d71d29df4ecf97dc3a29b1c
Nick Klassen <nick.klassen@smail.th-koeln.de>
2022-07-28 11:49:33 +0200

    created functions

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,34 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+
+function resizeElement(newName){
+
+    let image = document.querySelector("article[class = backpack] figure img");
+    let addedWidth = image.setAttribute(newName, 8);
+    
+};
+
+/* renameElement("width") */
+
+const anonymResize = function (newName) {
+
+    let image = document.querySelector("article[class = backpack] figure img");
+    let addedWidth = image.setAttribute(newName, 8);
+}
+
+/* anonymResize("width"); */
+
+
+const arrowResize = newName => {
+
+    let image = document.querySelector("article[class = backpack] figure img");
+    let addedWidth = image.setAttribute(newName, 8);
+}
+
+arrowResize("width");
+
+
+
+
